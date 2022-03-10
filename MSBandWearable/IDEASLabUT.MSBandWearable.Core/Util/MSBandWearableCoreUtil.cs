849df408e94f0268a23832445e1d6a3d24f8530b
Prakash Khadka <prakashkhadka@aol.com>
2022-03-10 09:15:36 -0500

    Added documentation for RunLaterInUIThread methods

--- MSBandWearable/IDEASLabUT.MSBandWearable.Core/Util/MSBandWearableCoreUtil.cs
+++ MSBandWearable/IDEASLabUT.MSBandWearable.Core/Util/MSBandWearableCoreUtil.cs
@@ -10,6 +10,12 @@ namespace IDEASLabUT.MSBandWearable.Core.Util
 {
     public static class MSBandWearableCoreUtil
     {
+        /// <summary>
+        /// Runs the given priority action in main core dispatcher thread asynchronously. Null action will not be invoked in core dispatcher thread
+        /// </summary>
+        /// <param name="action">An action to be invoked in core dispatcher thread</param>
+        /// <param name="coreDispatcherPriority">A core dispatcher priority for invoked action which defaults to normal</param>
+        /// <returns>A task that can be awaited</returns>
         public static async Task RunLaterInUIThread(Action action, CoreDispatcherPriority coreDispatcherPriority = CoreDispatcherPriority.Normal)
         {
             if (action == null)
@@ -18,17 +24,22 @@ namespace IDEASLabUT.MSBandWearable.Core.Util
             }
 
             var coreDispatcher = CoreApplication.MainView.CoreWindow.Dispatcher;
-
             if (coreDispatcher.HasThreadAccess)
             {
                 action.Invoke();
+                return;
             }
-            else
-            {
-                await coreDispatcher.RunAsync(coreDispatcherPriority, new DispatchedHandler(action));
-            }
+            await coreDispatcher.RunAsync(coreDispatcherPriority, new DispatchedHandler(action));
         }
 
+        /// <summary>
+        /// Runs the given priority action in main core dispatcher thread asynchronously by passing given input value
+        /// </summary>
+        /// <typeparam name="T">A parameter of type <see cref="BaseEvent"/> </typeparam>
+        /// <param name="action">An action to be invoked in core dispatcher thread</param>
+        /// <param name="inputValue">An input value for given action</param>
+        /// <param name="coreDispatcherPriority">A core dispatcher priority for invoked action which defaults to normal</param>
+        /// <returns>A task that can be awaited</returns>
         public static async Task RunLaterInUIThread<T>(Action<T> action, T inputValue, CoreDispatcherPriority coreDispatcherPriority = CoreDispatcherPriority.Normal) where T : BaseEvent
         {
             await RunLaterInUIThread(() => action.Invoke(inputValue), coreDispatcherPriority);
